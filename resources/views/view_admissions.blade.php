76ea09c34fa348d18333f4cccdcb24e057530ba4
Rkm11 <rupeshm173@gmail.com>
2018-10-19 18:56:09 +0530

    Update view_admissions.blade.php

--- resources/views/view_admissions.blade.php
+++ resources/views/view_admissions.blade.php
@@ -4,16 +4,58 @@ View Admissions
 @endsection
 @push('header')
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.4.2/css/buttons.dataTables.min.css">
+<style type="text/css">
+.table-responsive,
+.table .table-striped .table-bordered .w-auto{-sm|-md|-lg|-xl overflow-x:auto;
+}
+/*.dataTables_wrapper .dataTables_length {
+float: left;
+}
+.dataTables_wrapper .dataTables_filter {
+float: left;
+text-align: left;
+position: fixed;
+top: 150px;
+/*left: 330px;*/
+/*width: 30px;
+right: 220px;
+
+
+
+
+
+}*/*.dataTables_wrapper .dataTables_length {
+float: left;
+}
+.dataTables_wrapper .dataTables_filter {
+float: right;
+text-align: right;
+}
+.dataTables_wrapper .dataTables_length {
+float: right;
+}
+.dataTables_wrapper .dataTables_filter {
+float: right;
+text-align: left;
+}
+
+.enquiry-table{ overflow-x: auto; }
+
+
+
+
+</style>
 @endpush
 @section('content')
-<div class="col-lg-12">
-	<section class="box ">
-		<br>
+<div class="container-fluid">
+<!-- <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12"> -->
+	<!-- <section class="box">
+
 		<div class="content-body" style="background-color:#9ddac0;">
 			<div class="row">
-				<div class="col-xs-12">
+				<div class="col-xs-12"> -->
 					<div class="table-responsive">
-						<table class="table table-striped table-bordered" id="enquiry-table" >
+						<table class="table table-striped table-bordered " id="enquiry-table" >
 							<thead style="background-color:#fff;">
 								<tr>
 									<th>ID</th>
@@ -21,7 +63,7 @@ View Admissions
 									<th>Mobile</th>
 									<th>Parent</th>
 									<th>Parent No.</th>
-									<th>Admission Date</th>
+									<th>Standard</th>
 									<th>Admission</th>
 									<th>Action</th>
 									<th>Print</th>
@@ -33,13 +75,15 @@ View Admissions
 							</tbody>
 						</table>
 					</div>
-				</div>
+			<!-- 	</div>
 			</div>
 		</div>
-	</section>
+	</section> -->
+<!-- </div> -->
 </div>
 
-</div>
+
+
 <!-- END CONTAINER -->
 @endsection
 
@@ -61,18 +105,28 @@ View Admissions
 		 $('#enquiry-table').DataTable({
 
 			processing: true,
-			searching: true,
+
 			paging: false,
     	Filter: false,
+    	search:true,
+
+
+
+
+
+
+
+    	 // scrollX:true,
+
 			//serverSide: true,
 			ajax: '{!! route('admission.data') !!}',
 			columns: [
-			{data : 'ad_id', name : 'ad_id'},
+			{data : 'stu_uid', name : 'stu_uid'},
 			{data : 'stu_name', name : 'stu_name'},
 			{},
 			{data: 'parent_first_name', name: 'parent_first_name'},
 			{},
-			{data: 'ad_date', name: 'ad_date'},
+			{data: 'std_name', name: 'std_name'},
 			{},
 			{},
 			{},
@@ -90,7 +144,7 @@ View Admissions
 			},{
 				'targets': 4,
 				'render': function (data, type, full, meta){
-					if(full.parent_mobile == null || full.parent_mobile==0){
+					if(full.parent_mobile == null || full.parent_mobile ==0){
 						return '-';
 					}else{
 						return full.parent_mobile;
@@ -99,10 +153,10 @@ View Admissions
 			},{
 				'targets': 6,
 				'render': function (data, type, full, meta){
-					if(full.ad_status){
-						return '<span class="btn btn-primary">Confirmed</span>';
+					if(full.in_paid_amount){
+						return '<span class="btn btn-primary">Paid</span>';
 					}else{
-						return '<a class="btn btn-warning" href = "'+redA(full.ad_id)+'">Confirm Now</a>';
+						return '<a class="btn btn-warning" href = "'+redAFees(full.stu_id)+'">Admission Fees</a>';
 					}
 				}
 			},{
@@ -128,6 +182,10 @@ View Admissions
 	var v = '{{ url('admission/') }}';
 	function redA(id){
 		return v+'/'+id+'/edit';
+	}
+	function redAFees(id){
+		var v = '{{ url('invoice/') }}';
+		return v+'/'+id+'/edit?type=3';
 	}
 	function pr(id){
 		return "download-admission/"+id;
