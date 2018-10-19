f6e28378e3b9248faa77b68a475bce50be542bf6
Rkm11 <rupeshm173@gmail.com>
2018-10-19 14:59:14 +0530

    Update EnquiryController.php

--- app/Http/Controllers/EnquiryController.php
+++ app/Http/Controllers/EnquiryController.php
@@ -17,6 +17,7 @@ use Charts;
 use DataTables;
 use DB;
 use Illuminate\Http\Request;
+use PDF;
 
 class EnquiryController extends Controller {
 	public function __construct() {
@@ -203,5 +204,22 @@ class EnquiryController extends Controller {
 		$user->delete();
 
 		return redirect()->route('users.index');
+	}
+
+	public function print($id) {
+
+		$i = Enquiry::where('enq_id', $id)
+			->join('students', 'students.stu_id', '=', 'enquiries.enq_student')
+			->join('admission_details', 'admission_details.ad_id', '=', 'enquiries.enq_admission')
+			->join('standards', 'standards.std_id', '=', 'admission_details.ad_standard')
+			->join('parent_details', 'parent_details.parent_id', '=', 'students.stu_parent')
+			->join('mediums', 'mediums.med_id', '=', 'admission_details.ad_medium')
+			->first();
+		// dd($i);
+		// dd($rel);
+		// return view('reports.enquiry', compact('i', 'rel'));
+		$pdf = PDF::loadView('reports.enquiry', compact('i', 'rel'))->setPaper('a4')->setWarnings(false);
+		$pdf_name = 'enquiry-' . date('Y-m-d h:i:s') . '.pdf';
+		return $pdf->download($pdf_name);
 	}
 }
