43ff7755a13da9715a6a58fa7d0007659a5fb9d3
Rkm11 <rupeshm173@gmail.com>
2018-10-22 23:54:07 +0530

    Update create_telecalling.blade.php

--- resources/views/create_telecalling.blade.php
+++ resources/views/create_telecalling.blade.php
@@ -16,47 +16,47 @@ Add New
 <div class="col-lg-12">
 	<section class="box " style="background-color:#9ddac0;">
 		<div class="content-body" style="background-color:#9ddac0;">
-								
+
 				<div class="row">
 					<div class=" col-sm-12">
 						<br/>
 						<form class="form-horizontal" method="POST" action="{{route('telecalling.store')}}" >
 							<input type="hidden" name="_token" value="{{ csrf_token() }}">
-				        	
+
 				        	<div class="form-group">
 						    	<label for="add-name" class="col-sm-2 form-label">Student Name<span style="color:red;">*</span>:</label>
 						    	<div class="col-sm-9">
 						      		<input type="text" class="form-control" id="student_name" name="student_name" placeholder="Name" value="" required>
 						    	</div>
 						  	</div>
-						  
+
 						  	<div class="form-group">
 						    	<label for="add-mobile" class="col-sm-2 form-label">Mobile<span style="color:red;">*</span>:</label>
 						    	<div class="col-sm-9">
-						      		<input type="mobile" class="form-control" id="mobile" name="mobile" maxlength="10" pattern="[0-9]{10}" placeholder="Mobile number" value="" required>
+						      		<input type="mobile" class="form-control" id="mobile" name="mobile" maxlength="10" pattern="^[789]\d{9}$" placeholder="Mobile number" value="" required>
 						    	</div>
 						  	</div>
-						  
+
 					  	<div class="form-group">
 					    	<label for="follow1" class="col-sm-2 form-label">Follow Up1<span style="color:red;">*</span>:</label>
 					    	<div class="col-sm-9">
 					      		<textarea class="form-control" rows="8" wrap="follow1" id="follow1" name="follow1" placeholder="Statement here.." required></textarea>
 					    	</div>
 					  	</div>
-					  							
+
 						<div class="form-group">
 					    	<div class="col-sm-2">
-					    		
+
 		        				</div>
 		        				<div class="col-sm-10">
 		        				<button type="submit" class="btn btn-warning">Add</button>
 					    	</div>
-						</div>	
-						
+						</div>
+
 						</form>
 					</div>
 				</div>
-			
+
 		</div>
 	</section>
 </div>
